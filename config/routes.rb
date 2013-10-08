d590a5fb6483fa0991eed8d0850284b708b883c1
John Beene <beenetown@yahoo.com>
2013-10-07 20:20:20 -0400

    beginning cart creation

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Depot::Application.routes.draw do
+  resources :line_items
+
+  resources :carts
+
   get "store/index"
   resources :products
 
   # You can have the root of your site routed with "root"
-  root 'store#index'
+  root 'store#index', as: 'store' #, via: :a
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
